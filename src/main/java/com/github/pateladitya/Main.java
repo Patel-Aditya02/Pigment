21eaf2f1d54f5690e267fa70a0a5dc6f092aaf20
Aditya Patel <staradityapatel@gmail.com>
2026-09-07 01:52:04 +0530

    Deleted Main.java once again

--- src/main/java/com/github/pateladitya/Main.java
+++
@@ -1,7 +0,0 @@
-import com.github.pateladitya.Ink;
-import com.github.pateladitya.Paint;
-import com.github.pateladitya.Style;
-
-void main() {
-    IO.println(Paint.of("Test").style(Style.UNDERLINE).fg(Ink.BLACK).bg(Ink.CYAN).style(Style.ITALIC));
-}
