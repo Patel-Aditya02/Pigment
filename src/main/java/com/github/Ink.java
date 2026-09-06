9f0b41534d68b3fb602b103a2a2f6cfff794f255
Aditya Patel <staradityapatel@gmail.com>
2026-09-07 00:05:55 +0530

    Style and Ink complete

--- src/main/java/com/github/Ink.java
+++ src/main/java/com/github/Ink.java
@@ -1,15 +1,21 @@
 package com.github;
 
 public enum Ink {
-    RED("",""),
-    BLACK("", ""),
-    WHITE("", ""),
-    GREY("", "");
+    BLACK("\033[30m", "\033[40m"),
+    RED("\033[31m","\033[41m"),
+    GREEN("\033[32m", "\033[42m"),
+    YELLOW("\033[33m", "\033[43m"),
+    BLUE("\033[34m", "\033[44m"),
+    MAGENTA("\033[35m", "\033[45m"),
+    CYAN("\033[36m", "\033[46m"),
+    WHITE("\033[37m", "\033[47m"),
+    GREY("\033[38;5;245m", "\033[48;5;245m");
 
     private final String fg;
     private final String bg;
 
-    public static final String RESET = "\\033[0m";
+    public static final String RESET_FG = "\033[39m";
+    public static final String RESET_BG = "\033[49m";
 
     Ink(String fg, String bg) {
         this.fg = fg;
@@ -17,11 +23,19 @@ public enum Ink {
     }
 
     public String fg(String str){
-        return this.fg + str + RESET;
+        return this.fg + str + RESET_FG;
     }
 
     public String bg(String str){
-        return this.bg + str;
+        return this.bg + str + RESET_BG;
+    }
+
+    public void print(Object o){
+        IO.print(this.fg(o.toString()));
+    }
+
+    public void printBg(Object o){
+        IO.print(this.bg(o.toString()));
     }
 
     public void println(Object o){
---
+++ src/main/java/com/github/Style.java
@@ -0,0 +1,35 @@
+package com.github;
+
+public enum Style {
+    BOLD("\033[1m", "\033[22m"),
+    DIM("\033[2m", "\033[22m"),
+    ITALIC("\033[3m", "\033[23m"),
+    UNDERLINE("\033[4m", "\033[24m"),
+    BLINK_SLOW("\033[5m", "\033[25m"),
+    BLINK_FAST("\033[6m", "\033[25m"),
+    INVERT("\033[7m", "\033[27m"),
+    HIDDEN("\033[8m", "\033[28m"),
+    STRIKE_THROUGH("\033[9m", "\033[29m");
+
+    private final String ansi;
+    private final String reset;
+
+    public static final String RESET = "";
+
+    Style(String ansi, String reset){
+        this.ansi = ansi;
+        this.reset = reset;
+    }
+
+    public String apply(String str){
+        return this.ansi + str + this.reset;
+    }
+
+    public void print(Object o){
+        IO.print(this.apply(o.toString()));
+    }
+
+    public void println(Object o){
+        IO.println(this.apply(o.toString()));
+    }
+}
